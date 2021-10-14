061457b709a68b67bb41e42b2ec985d52f37ec9a
LAPTOP-BLDQ38QR\asus <rememberme752@gmail.com>
2021-10-14 09:26:21 +0700

    Initial commit

---
+++ app/src/main/java/com/rememberdev/fragmentbottomnavigation/MainActivity.java
@@ -0,0 +1,47 @@
+package com.rememberdev.fragmentbottomnavigation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Menjadikan Fragment Chats menjadi default
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ChatsFragment()).commit();
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.nav_chats:
+                        selectedFragment = new ChatsFragment();
+                        break;
+                    case R.id.nav_status:
+                        selectedFragment = new StatusFragment();
+                        break;
+                    case  R.id.nav_calls:
+                        selectedFragment = new CallsFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                return true;
+            }
+        });
+    }
+}
